e917385360ca5d505163def50c2f482dc69afcd5
Vojislav Zaja <81269127+vojaso82@users.noreply.github.com>
2021-09-23 10:44:43 -0400

    footer done

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,10 @@
-import React from 'react'
-import './Footer.css'
+import React from 'react';
+import './Footer.css';
 import { HashLink } from 'react-router-hash-link';
+import { Link } from 'react-router-dom';
+import * as AiIcons from 'react-icons/ai';
+import * as FaIcons from 'react-icons/fa';
+import * as GrIcons from 'react-icons/gr';
 
 function Footer() {
     return (
@@ -8,7 +12,13 @@ function Footer() {
                 <div className='div1'>
                 <HashLink className='button-home' smooth to={'/#home'}><button>^</button></HashLink> 
                 <p>2021 © Vojislav Zaja, All rights reserved.</p>
-                </div>  
+                </div> 
+                <div className='social-icons'>
+                <a className='icons' href="https://www.facebook.com/" target='_blank'><FaIcons.FaFacebookSquare/></a>
+                <a className='icons' href="https://twitter.com/" target='_blank'><FaIcons.FaTwitterSquare/></a>
+                <a href="https://www.instagram.com/" target='_blank'><AiIcons.AiFillInstagram/></a>
+                <a href="https://www.linkedin.com/" target='_blank'><AiIcons.AiFillLinkedin/></a>
+                </div> 
         </div>
     )
 }
